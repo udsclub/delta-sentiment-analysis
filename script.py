ff7471d4bbda4166069f355bb88ee7167dc0e8d5
annKhan <hannakhanko@gmail.com>
2017-02-23 19:28:10 +0200

    adapting the coding style to pep8

--- script.py
+++ script.py
@@ -11,28 +11,34 @@ from sklearn.naive_bayes import MultinomialNB
 
 
 def load_data():
-    df = pd.read_csv('reviews_rt_all.csv', header=0, error_bad_lines=False, delimiter='|')
+    # load dataset into pandas dataframe
+    df = pd.read_csv('reviews_rt_all.csv', header=0,
+                     error_bad_lines=False, delimiter='|')
     return df
 
 
 # data pre-processing
 def tokenize_text(text):
     # delete punctuation symbols
-    text_without_punc = ''.join(s for s in text if s not in string.punctuation)
+    punc_symb = string.punctuation
+    text_no_punc = ''.join(s for s in text if s not in punc_symb)
     # tokenize text
-    tokenize_text = word_tokenize(text_without_punc)
+    token_text = word_tokenize(text_no_punc)
+    # delete stop-words
     stop_list = set(stopwords.words('english'))
-    x_without_stop = [word for word in tokenize_text if word.lower() not in stop_list and not word.lower().isdigit()]
+    clean_text = [w for w in token_text if w.lower() not in stop_list]
     # stemming
     stemmer = SnowballStemmer("english")
-    result_text = [stemmer.stem(word) for word in x_without_stop]
+    result_text = [stemmer.stem(word) for word in clean_text]
     return result_text
 
 
-# building feature vectors
+# feature extraction
 def build_feature_matrices(df):
-    print("Extracting features from the training dataset using a Tfidf vectorizer")
+    print("Feature extraction using a Tfidf vectorizer")
+    # build a vocabulary that only consider the top 200 features
     vectorizer = TfidfVectorizer(tokenizer=tokenize_text, max_features=200)
+    # learn vocabulary and return term-document matrix
     X = vectorizer.fit_transform(df['text'].values)
     X_array = X.toarray()
     print("n_samples: %d, n_features: %d" % X.shape)
@@ -41,9 +47,13 @@ def build_feature_matrices(df):
 
 # build classifier
 def build_clf(X, y):
+    # split dataset into random train and test subsets,
+    # the proportion of the dataset to include in the test split - 0.33
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
     clf = MultinomialNB()
+    # fit Naive Bayes classifier according to X, y
     clf.fit(X_train, y_train)
+    # return the mean accuracy on the given test data and labels
     score = clf.score(X_test, y_test)
     print('Accuracy: %s' % (score))
 
@@ -52,6 +62,6 @@ def main():
     df = load_data()
     y = df.label.values
     X = build_feature_matrices(df)
-    build_clf(X,y)
+    build_clf(X, y)
 
 main()
